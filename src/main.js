1fab85b394a50cec436bf45c6689758f120ce71e
Hugo Josefson <hugo@josefson.org>
2015-12-02 13:25:50 +0100

    Cleanup, format src.

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import Cycle from '@cycle/core';
 import CycleDOM from '@cycle/dom';
 
 function main() {
-  // ...
-  console.log(Cycle, CycleDOM)
-  return {
-    DOM: Rx.Observable.interval(1000)
-    .map(i => CycleDOM.h('h1', '' + i + ' seconds elapsed.'))
-  }
+    // ...
+    console.log(Cycle, CycleDOM);
+    return {
+        DOM: Rx.Observable.interval(1000)
+            .map(i => CycleDOM.h('h1', `${i} seconds elapsed.`))
+    }
 }
 
-let drivers = {
-  DOM: CycleDOM.makeDOMDriver('#app')
+const drivers = {
+    DOM: CycleDOM.makeDOMDriver('#app')
 };
 
 Cycle.run(main, drivers);
